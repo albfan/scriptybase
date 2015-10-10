3d22f7d749e95e87ada8dcb5d90cdc3e75bda00f
albfan <albertofanjul@gmail.com>
2015-10-10 08:30:11 +0200

    empty sqlite test db

---
+++ test/sqlite/patch1.sql
@@ -0,0 +1,4 @@
+create table memos(text, priority INTEGER);
+insert into memos values('deliver project description', 10);
+insert into memos values('lunch with Christine', 100);
+select * from memos;
